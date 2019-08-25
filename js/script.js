b23efaba7ba3b1a83afbcbeebf0451142fdc4c34
NathanRomanos <nathanromanos@hotmail.com>
2019-08-26 11:30:24 +1200

    drinks

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
   })
 
 // menu change
-  $('#change').click(function(){
+  $('.change').click(function(){
     $('.menu__change').fadeToggle(200);
   })
 
@@ -25,42 +25,62 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('#sp').click(function(){
     $('.menu__content--SP, .menu__header--SP').removeClass("inactive").addClass( "active" );
+    $('.menu__dietryInfo').removeClass("inactive").addClass( "active" );
     $('.menu__content--LB, .menu__header--LB').removeClass("active").addClass( "inactive" );
     $('.menu__content--DE, .menu__header--DE').removeClass("active").addClass( "inactive" );
     $('.menu__content--SI, .menu__header--SI').removeClass("active").addClass( "inactive" );
     $('.menu__content--DI, .menu__header--DI').removeClass("active").addClass( "inactive" );
+    $('.menu__content--DR, .menu__header--DR').removeClass("active").addClass( "inactive" );
   })
 
   $('#lb').click(function(){
     $('.menu__content--LB, .menu__header--LB').removeClass("inactive").addClass( "active" );
+    $('.menu__dietryInfo').removeClass("inactive").addClass( "active" );
     $('.menu__content--SP, .menu__header--SP').removeClass("active").addClass( "inactive" );
     $('.menu__content--DE, .menu__header--DE').removeClass("active").addClass( "inactive" );
     $('.menu__content--SI, .menu__header--SI').removeClass("active").addClass( "inactive" );
     $('.menu__content--DI, .menu__header--DI').removeClass("active").addClass( "inactive" );
+    $('.menu__content--DR, .menu__header--DR').removeClass("active").addClass( "inactive" );
   })
 
   $('#de').click(function(){
     $('.menu__content--DE, .menu__header--DE').removeClass("inactive").addClass( "active" );
+    $('.menu__dietryInfo').removeClass("inactive").addClass( "active" );
     $('.menu__content--LB, .menu__header--LB').removeClass("active").addClass( "inactive" );
     $('.menu__content--SP, .menu__header--SP').removeClass("active").addClass( "inactive" );
     $('.menu__content--SI, .menu__header--SI').removeClass("active").addClass( "inactive" );
     $('.menu__content--DI, .menu__header--DI').removeClass("active").addClass( "inactive" );
+    $('.menu__content--DR, .menu__header--DR').removeClass("active").addClass( "inactive" );
   })
 
   $('#si').click(function(){
     $('.menu__content--SI, .menu__header--SI').removeClass("inactive").addClass( "active" );
+    $('.menu__dietryInfo').removeClass("inactive").addClass( "active" );
     $('.menu__content--LB, .menu__header--LB').removeClass("active").addClass( "inactive" );
     $('.menu__content--SP, .menu__header--SP').removeClass("active").addClass( "inactive" );
     $('.menu__content--DE, .menu__header--DE').removeClass("active").addClass( "inactive" );
     $('.menu__content--DI, .menu__header--DI').removeClass("active").addClass( "inactive" );
+    $('.menu__content--DR, .menu__header--DR').removeClass("active").addClass( "inactive" );
   })
 
   $('#di').click(function(){
     $('.menu__content--DI, .menu__header--DI').removeClass("inactive").addClass( "active" );
+    $('.menu__dietryInfo').removeClass("inactive").addClass( "active" );
     $('.menu__content--LB, .menu__header--LB').removeClass("active").addClass( "inactive" );
     $('.menu__content--SP, .menu__header--SP').removeClass("active").addClass( "inactive" );
     $('.menu__content--DE, .menu__header--DE').removeClass("active").addClass( "inactive" );
     $('.menu__content--SI, .menu__header--SI').removeClass("active").addClass( "inactive" );
+    $('.menu__content--DR, .menu__header--DR').removeClass("active").addClass( "inactive" );
+  })
+
+  $('#dr').click(function(){
+    $('.menu__content--DR, .menu__header--DR').removeClass("inactive").addClass( "active" );
+    $('.menu__dietryInfo').removeClass("active").addClass( "inactive" );
+    $('.menu__content--LB, .menu__header--LB').removeClass("active").addClass( "inactive" );
+    $('.menu__content--SP, .menu__header--SP').removeClass("active").addClass( "inactive" );
+    $('.menu__content--DE, .menu__header--DE').removeClass("active").addClass( "inactive" );
+    $('.menu__content--SI, .menu__header--SI').removeClass("active").addClass( "inactive" );
+    $('.menu__content--DI, .menu__header--DI').removeClass("active").addClass( "inactive" );
   })
 
 });
